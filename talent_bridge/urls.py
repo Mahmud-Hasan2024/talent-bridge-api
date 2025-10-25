76a3ed0dff14197568bb3723a328693abcebb8b1
Mahmud Hasan <mh.kid234@gmail.com>
2025-10-25 11:41:06 +0600

    added home page redirection api root view at /api/v1/auth

--- talent_bridge/urls.py
+++ talent_bridge/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from talent_bridge.views import api_root_view
 
 
 schema_view = get_schema_view(
@@ -38,7 +39,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
+    path('', api_root_view),
+    path('api/v1/', include('api.urls'), name='api-root'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
